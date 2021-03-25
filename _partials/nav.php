81c5ddd3aa7143452217c9958d34afcd0fae1b4a
ThamTran <thihanhtham.tran@gmail.com>
2021-03-25 10:38:18 +0100

    finished

--- _partials/nav.php
+++ _partials/nav.php
@@ -2,33 +2,36 @@
       <a class="navbar-brand" href="#">Accueil</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-    </button>
+      </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
 
-      <?php  if(!isset($_SESSION['user_id']) && !isset($_SESSION['pseudo'])) 
-      { ?>
-          <li class="nav-item active">
-            <a class="nav-link" href="?page=register">Inscription <span class="sr-only">(current)</span></a>
-        </li>
+          <?php if (!isset($_SESSION['user_id']) && !isset($_SESSION['pseudo'])) { ?>
+            <li class="nav-item active">
+              <a class="nav-link" href="?page=register">Inscription <span class="sr-only">(current)</span></a>
+            </li>
 
 
-        <li class="nav-item">
-            <a class="nav-link" href="?page=login">Connexion</a>
-        </li>
+            <li class="nav-item">
+              <a class="nav-link" href="?page=login">Connexion</a>
+            </li>
 
-      <?php } else { ?>
+          <?php } else { ?>
 
-        <li class="nav-item">
-            <a class="nav-link" href="?page=deconnexion">Déconnexion</a>
-        </li>
-      <?php } //on doit tjr ouvrir et fermer pour que html ne s'affiche que qd il repond a la condition ?>
-      
-</ul>
-<form class="form-inline my-2 my-lg-0">
-  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-</form>
-</div>
-</nav>
+            <li class="nav-item">
+              <a class="nav-link" href="?page=modif_mdp">Modification mdp</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="?page=deconnexion">Déconnexion</a>
+            </li>
+          <?php } //on doit tjr ouvrir et fermer pour que html ne s'affiche que qd il repond a la condition 
+          ?>
+
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
--- views/index.view.php
+++ views/index.view.php
@@ -1,16 +1,16 @@
-
-<?php   ob_start()  ?>
+<?php ob_start()  ?>
 
 <div class="jumbotron">
   <h1 class="display-4">Hello, world!</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p>extract($variable, EXTR_SKIP);</p>
+  <p>array_map('htmlspecialchars', $_POST);</p>
+  <p>session_destroy();</p>
+  <p>$_SESSION=[]</p>
   <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
 </div>
 
 
-<?php  $content = ob_get_clean();  ?>
+<?php $content = ob_get_clean();  ?>
 
 
-<?php  require_once 'views/gabarit.php'; ?>
+<?php require_once 'views/gabarit.php'; ?>
